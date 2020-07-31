48dcf2b4769846c51281a5b11372c34b8fd69799
Franco <franco.accifonte@widergy.com>
2020-07-31 20:08:46 -0300

    changed a bit, its kind of a mess

--- stuff/stuff/spiders/jobs.py
+++ stuff/stuff/spiders/jobs.py
@@ -12,36 +12,27 @@ import json
 class JobsSpider(scrapy.Spider):
     name = "Jobs"
     configure_logging(install_root_handler=False)
-    logging.basicConfig(
-        filename='log.txt',
-        format='%(levelname)s: %(message)s',
-        level=logging.INFO
-    )
 
     def start_requests(self):
-        driver = webdriver.Chrome('./support/chromedriver.exe')
-        search_url = 'https://www.linkedin.com/jobs/'
+        driver = webdriver.Chrome('./support/chromedriver_linux')
+        search_url0 = 'https://www.linkedin.com/jobs/'
         query = 'Python'
         place = 'Germany'
-        jobs_to_scrape = 4
+        search_url = 'https://www.linkedin.com/jobs/search?keywords=' + query.lower() + '&location=' + place.lower()
+        jobs_to_scrape = 20
 
         driver.get(search_url)
         driver.find_element_by_xpath('//*[@id="JOBS"]/section[1]/input').send_keys(query)
         driver.find_element_by_xpath('//*[@id="JOBS"]/section[2]/input').clear()
         driver.find_element_by_xpath('//*[@id="JOBS"]/section[2]/input').send_keys(place + Keys.ENTER)
         search_result_ammount = driver.find_element_by_xpath('//*[@id="main-content"]/div/section/div[2]/h1/span[1]').text
-        # urlsss = driver.find_element_by_xpath('//*[@id="main-content"]/div/section/ul/li[a]/a').get_attribute('href')
-        pdb.set_trace()
-        for ii in range(1, jobs_to_scrape + 1):
-            self.index = ii
-            url = driver.find_element_by_xpath('//*[@id="main-content"]/div/section/ul/li[' + str(ii) + ']/a').get_attribute('href').copy()
-            yield scrapy.Request(url=url, callback=self.parse)
+        urls = []
+        for jj in range(1, jobs_to_scrape + 1):
+          yield driver.find_element_by_xpath('//*[@id="main-content"]/div/section/ul/li[' + str(jj) + ']/a').get_attribute('href')
+            
             
     def parse(self, response):
-        # pdb.set_trace()
-        logging.debug('Indice Numero: %s' % self.index)
-        text = remove_tags(response.selector.xpath('/html/body/main/section[1]/section[3]/div/section/div').get())
+        text = response.selector.xpath('/html/body/main/section[1]/section[3]/div/section/div/text()').get()
         filename = './stuff/results/jobs-%s.json' % self.index
         data = { 'url': response.url, 'text': text }
-        with open(filename, 'w') as f:
-            json.dump(data, f, indent=4)
+        yield data
